b8f1bbdf2dd7d40199a7037a71708d0de6b6be7a
SuriNathalie <65183340+SuriNathalie@users.noreply.github.com>
2020-05-24 15:00:52 +0200

    kommis dazu

--- Buttons.py
+++ Buttons.py
@@ -5,29 +5,47 @@ Created on Tue May 19 10:18:54 2020
 @author: Suri
 """
 
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 21:17:23 2020
+
+@author: Suri
+"""
 from SocialDistancing import *
 
+
+#pygame is initiated
 pygame.init()
+#window is created
 screen = pygame.display.set_mode((600, 400))
 font = pygame.font.SysFont('Arial', 20)
+#
 active = False
 lockdownFlag = False
 
-
+#textobjects are defined 
 def textobjects(text, font):
+    #text surface is created, font is black
     textsurface = font.render(text, True, (0, 0, 0))
+    #a rectangle with the size of the text is created
     return textsurface, textsurface.get_rect()
 
-
+#buttons are defined, they're dependant on an x and y position,a label, a length, height, two colours and a border
 def button(x, y, label, length, height, standard_colour, active_colour, border):
     global active
     global lockdownFlag
+    #gives mouse position as (x,y)
     mouse = pygame.mouse.get_pos()
+    #gives information about the wether the mouse is clicked or not(as tipel with three entries)
     click = pygame.mouse.get_pressed()
+    #checks wether the mouse cursor is inside the button 
     if x < mouse[0] < x + length and y < mouse[1] < y + height:
+        #button ist actually created in a lighter colour to show that the cursor is on the button
         pygame.draw.rect(screen, active_colour, (x, y, length, height))
+        #checks wether the left mouse button is pressed and the button is already active
         if click[0] == 1 and not active:
             active = True
+            #buttons get different methods, dependant on their label
             if label == "Standard Simulation":
                 population1 = initialize(popSize=popsize1, infectedPercentage=iPerc, sickPercentage=sPerc,
                                          xlowerlimit=xlowerlim,
@@ -65,17 +83,22 @@ def button(x, y, label, length, height, standard_colour, active_colour, border):
                 simulation(population, lockdownFlag)
 
 
-
+            #exit button is initiated
+            #if exit is pressed, pygame ends
             elif label == "Exit":
                 # run = False
                 pygame.quit()
             elif label == "Lockdown":
                 lockdownFlag = True
+        #
         if click[0] == 0:
             active = False
+    #if the mouse cursor is not inside the button limits, the button is drawn in a darker colour
     else:
         pygame.draw.rect(screen, standard_colour, (x, y, length, height))
+    #the border that surrounds the button is created(black)
     pygame.draw.rect(screen, (0, 0, 0), (x, y, length, height), border)
     textground, textbox = textobjects(label, font)
     textbox.center = (int(np.floor((x + (length / 2)))), int(np.floor(y + (height / 2))))
+    #the text is displayed on the textsurface
     screen.blit(textground, textbox)
